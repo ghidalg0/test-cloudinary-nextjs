f10ebb1f5c0164038df3e4847754c75a9a7284e7
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-10-20 20:05:43 +0200

    working with preview

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
@@ -5,6 +6,7 @@ export default function Home() {
   const url = "https://api.cloudinary.com/v1_1/dwb8y1to0/image/upload";
 
   const [chosenFile, setChosenFile] = useState();
+  const [tempURL, setTempURL] = useState();
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
@@ -22,6 +24,9 @@ export default function Home() {
     e.preventDefault();
     const selectedFile = e.target.value.replace("C:\\fakepath\\", "");
     setChosenFile(selectedFile);
+    const file = document.querySelector("[type=file]").files[0];
+    const fakeURL = URL.createObjectURL(file);
+    setTempURL(fakeURL);
   };
 
   return (
@@ -41,7 +46,14 @@ export default function Home() {
             onChange={onChange}
           />
         </label>
-        <div>{chosenFile}</div>
+        <p>{chosenFile}</p>
+        <Image
+          src={tempURL}
+          width={200}
+          height={200}
+          alt="Votre photo"
+          className={styles.image}
+        />
         <button type="submit">Ajouter</button>
       </form>
     </div>
